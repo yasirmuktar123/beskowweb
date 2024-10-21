d17d4dd1f7fdde1acbbc07f1d6a92188ab1fd3f3
Yasir Muktar <yasmuk@elevad.umea.se>
2024-10-21 13:18:15 +0200

    start

--- docs/_app/immutable/entry/start.DJMxc2LF.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.BYcFy7t5.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,27 @@
 <main>
   <div>
 
-    <h1> Yasir Muktar 2 </h1>
-    <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
-    <h3>Inspirerade citat för min webbutvecklingsresa:</h3>
+    <h1 class="name"> Yasir Muktar 2 </h1>
+    <h2 class="item">Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
+    <h3 id="number">life is roblox:</h3>
+    <p id="inspo">
+      "The only way to learn a new programming language is by writing programs in it." - <strong>Dennis Ritchie</strong>, creator of the C programming language.
+  </p>
+  
+  <p style="background-color:black">
+      "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it." - <span>Patrick McKenzie, software engineer and entrepreneur</span>.
+  </p>
     <!--Under denna kommer vi lägga till fler element-->
 
   </div>
     <!--Här kommer vi lägga till en bild senare i uppgiften 1.2.4 Extra styling-->  
+  
+<svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">
+  <path fill="#FF0066" d="M29.5,-44C37.1,-41,41.4,-30.9,43.5,-21.3C45.7,-11.7,45.8,-2.5,46.6,8.2C47.5,19,49.1,31.5,43.9,38.7C38.7,45.9,26.6,47.8,14.3,53.8C1.9,59.9,-10.8,70,-18.4,66.2C-26,62.4,-28.6,44.7,-40.2,33.5C-51.8,22.4,-72.5,17.9,-75.7,9.6C-78.8,1.3,-64.3,-10.7,-56.1,-24.3C-47.8,-37.9,-45.8,-53,-37.5,-55.7C-29.2,-58.3,-14.6,-48.4,-1.8,-45.6C10.9,-42.7,21.9,-46.9,29.5,-44Z" transform=" translate(100,70)"  />
+</svg>
 
 </main>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,160L0,192L36.9,192L36.9,128L73.8,128L73.8,96L110.8,96L110.8,32L147.7,32L147.7,64L184.6,64L184.6,224L221.5,224L221.5,192L258.5,192L258.5,320L295.4,320L295.4,256L332.3,256L332.3,64L369.2,64L369.2,64L406.2,64L406.2,32L443.1,32L443.1,192L480,192L480,0L516.9,0L516.9,192L553.8,192L553.8,128L590.8,128L590.8,224L627.7,224L627.7,96L664.6,96L664.6,32L701.5,32L701.5,64L738.5,64L738.5,160L775.4,160L775.4,256L812.3,256L812.3,288L849.2,288L849.2,128L886.2,128L886.2,0L923.1,0L923.1,32L960,32L960,96L996.9,96L996.9,96L1033.8,96L1033.8,224L1070.8,224L1070.8,192L1107.7,192L1107.7,64L1144.6,64L1144.6,256L1181.5,256L1181.5,256L1218.5,256L1218.5,160L1255.4,160L1255.4,256L1292.3,256L1292.3,320L1329.2,320L1329.2,288L1366.2,288L1366.2,288L1403.1,288L1403.1,192L1440,192L1440,320L1403.1,320L1403.1,320L1366.2,320L1366.2,320L1329.2,320L1329.2,320L1292.3,320L1292.3,320L1255.4,320L1255.4,320L1218.5,320L1218.5,320L1181.5,320L1181.5,320L1144.6,320L1144.6,320L1107.7,320L1107.7,320L1070.8,320L1070.8,320L1033.8,320L1033.8,320L996.9,320L996.9,320L960,320L960,320L923.1,320L923.1,320L886.2,320L886.2,320L849.2,320L849.2,320L812.3,320L812.3,320L775.4,320L775.4,320L738.5,320L738.5,320L701.5,320L701.5,320L664.6,320L664.6,320L627.7,320L627.7,320L590.8,320L590.8,320L553.8,320L553.8,320L516.9,320L516.9,320L480,320L480,320L443.1,320L443.1,320L406.2,320L406.2,320L369.2,320L369.2,320L332.3,320L332.3,320L295.4,320L295.4,320L258.5,320L258.5,320L221.5,320L221.5,320L184.6,320L184.6,320L147.7,320L147.7,320L110.8,320L110.8,320L73.8,320L73.8,320L36.9,320L36.9,320L0,320L0,320Z"></path></svg>
 
 <style>
   main{
@@ -24,6 +36,52 @@
     grid-template-columns: repeat(2,50%);
     overflow: hidden;
   }
+    
+h1{
+    color:grey;
+    background-color: lightblue;
+}
+h2{
+    color: rgb(255, 165, 0);
+}
+h3{
+    color: #3cb371;
+    font-family: sans-serif;
+    font-weight: bold;
+}
+.name{
+    font-family:'Gill Sans';
+    font-size: 30px;
+}
+
+
+.item{
+    width: 100px;
+    height: 50;
+}
+                
+
+#number{
+    border: 1px solid black;
+    padding: 10px;
+}
+#inspo{
+    font-family: courier;
+    font-size: 160%;
+}
+
+span{
+    color: green;
+}
+  
+
+  
+  h1, h2, h3, p {
+  display: block;
+  
+  margin: 10px;
+}
+        
   div{
     /*Styling för att centrera texten i behållaren*/
     margin: auto;
